9afe561dcd56501e1db23ba772a28d3b62c7f129
ape06 <zhenhao.li@kellogg.ox.ac.uk>
2023-04-24 14:18:58 +0100

    Add cambridge city in location service

--- chalicelib/location_service.py
+++ chalicelib/location_service.py
@@ -3,5 +3,6 @@ def information_about(place):
         return "London is the capital and largest city of England and the United Kingdom"
     if place == "Oxford":
         return "Oxford is home to the University of Oxford, the oldest university in the English-speaking world"
-
+    if place == "Cambridge":
+        return "Cambridge is home to the University of Cambridge, the second-oldest university in the English-speaking world"
     return f"Unknown place: {place}"
